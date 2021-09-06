0ea2cd5af12514c556edf94aa3b6f2491c50734a
voidGlitch <official.shreshthav@gmail.com>
2021-09-06 21:27:23 +0530

    Updates

---
+++ precache-manifest.b90ba9880b925904e94e92b530f66345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11088851007faf851dacf0712143e8b5",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "b7df8382f5dafab48ae9",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "e6a4d594860af0f4fa80",
+    "url": "/box-office/static/js/2.a23c53ea.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.a23c53ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7df8382f5dafab48ae9",
+    "url": "/box-office/static/js/main.37651b01.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
